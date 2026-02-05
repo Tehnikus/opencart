4506bb8d30379af64bf3138b7b12846a4cdc70e8
Tehnikus <tehnikus.ua@gmail.com>
2026-02-06 01:25:34 +0200

    Display category associatied stores in admin category list

--- upload/admin/controller/catalog/category.php
+++ upload/admin/controller/catalog/category.php
@@ -214,6 +214,9 @@ class ControllerCatalogCategory extends Controller {
 			);
 		}
 
+		$this->load->model('setting/store');
+		$data['stores'] = $this->model_setting_store->getMultistores();
+
 		if (isset($this->error['warning'])) {
 			$data['error_warning'] = $this->error['warning'];
 		} else {
