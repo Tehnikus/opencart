55633b3d025022da401feb0edfe1b335b4fa2fe1
Tehnikus <tehnikus.ua@gmail.com>
2026-02-06 01:26:30 +0200

    Add some extra data to admin category list for convenience

--- upload/admin/controller/catalog/category.php
+++ upload/admin/controller/catalog/category.php
@@ -206,11 +206,13 @@ class ControllerCatalogCategory extends Controller {
 
 		foreach ($results as $result) {
 			$data['categories'][] = array(
-				'category_id' => $result['category_id'],
-				'name'        => $result['name'],
-				'sort_order'  => $result['sort_order'],
-				'edit'        => $this->url->link('catalog/category/edit', 'user_token=' . $this->session->data['user_token'] . '&category_id=' . $result['category_id'] . $url, true),
-				'delete'      => $this->url->link('catalog/category/delete', 'user_token=' . $this->session->data['user_token'] . '&category_id=' . $result['category_id'] . $url, true)
+				'category_id' 		=> $result['category_id'],
+				'name'        		=> $result['name'],
+				'sort_order'  		=> $result['sort_order'],
+				'stores'  				=> $result['stores'],
+				'product_count'  	=> $result['product_count'],
+				'edit'        		=> $this->url->link('catalog/category/edit', 'user_token=' . $this->session->data['user_token'] . '&category_id=' . $result['category_id'] . $url, true),
+				'delete'      		=> $this->url->link('catalog/category/delete', 'user_token=' . $this->session->data['user_token'] . '&category_id=' . $result['category_id'] . $url, true)
 			);
 		}
 
