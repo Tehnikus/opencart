39ba2d4d74a68151b9f73328e82a2624891913b5
Tehnikus <tehnikus.ua@gmail.com>
2026-02-09 12:56:59 +0200

    Add store_id to getProductOptions()

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -1171,12 +1171,33 @@ class ModelCatalogProduct extends Model {
 	public function getProductOptions($product_id) {
 		$product_option_data = array();
 
-		$product_option_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "product_option` po LEFT JOIN `" . DB_PREFIX . "option` o ON (po.option_id = o.option_id) LEFT JOIN `" . DB_PREFIX . "option_description` od ON (o.option_id = od.option_id) WHERE po.product_id = '" . (int)$product_id . "' AND od.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY o.sort_order ASC");
+		$product_option_query = $this->db->query("
+			SELECT 
+				* 
+			FROM `" . DB_PREFIX . "product_option` po 
+			LEFT JOIN `" . DB_PREFIX . "option` o ON po.option_id = o.option_id 
+			LEFT JOIN " . DB_PREFIX . "option_to_store o2s on o2s.option_id = po.option_id 
+				AND o2s.store_id = po.store_id
+			LEFT JOIN `" . DB_PREFIX . "option_description` od ON o.option_id = od.option_id
+			WHERE po.product_id 	= '" . (int) $product_id . "' 
+				AND od.language_id 	= '" . (int) $this->config->get('config_language_id') . "' 
+				AND o2s.store_id 			= '" . (int) $this->session->data['store_id'] . "'
+				AND od.store_id 		= '" . (int) $this->session->data['store_id'] . "'
+			ORDER BY o.sort_order ASC
+		");
 
 		foreach ($product_option_query->rows as $product_option) {
 			$product_option_value_data = array();
 
-			$product_option_value_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_option_value pov LEFT JOIN " . DB_PREFIX . "option_value ov ON(pov.option_value_id = ov.option_value_id) WHERE pov.product_option_id = '" . (int)$product_option['product_option_id'] . "' ORDER BY ov.sort_order ASC");
+			$product_option_value_query = $this->db->query("
+				SELECT 
+					* 
+				FROM " . DB_PREFIX . "product_option_value pov 
+				LEFT JOIN " . DB_PREFIX . "option_value ov ON(pov.option_value_id = ov.option_value_id) 
+				WHERE pov.product_option_id = '" . (int) $product_option['product_option_id'] . "' 
+					AND ov.store_id 					= '" . (int) $this->session->data['store_id'] . "'
+					AND pov.store_id 					= '" . (int) $this->session->data['store_id'] . "'
+				ORDER BY ov.sort_order ASC");
 
 			foreach ($product_option_value_query->rows as $product_option_value) {
 				$product_option_value_data[] = array(
