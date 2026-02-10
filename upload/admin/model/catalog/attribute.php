568702b990d7f21827c362a2f3f2a36f3879faaa
Tehnikus <tehnikus.ua@gmail.com>
2026-02-10 16:04:48 +0200

    addAttribute()  multistore overhaul

--- upload/admin/model/catalog/attribute.php
+++ upload/admin/model/catalog/attribute.php
@@ -1,15 +1,57 @@
 <?php
 class ModelCatalogAttribute extends Model {
-	public function addAttribute($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "attribute SET attribute_group_id = '" . (int)$data['attribute_group_id'] . "', sort_order = '" . (int)$data['sort_order'] . "'");
+	public function addAttribute($data) : int {
 
-		$attribute_id = $this->db->getLastId();
+		$this->db->query("START TRANSACTION");
 
-		foreach ($data['attribute_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO " . DB_PREFIX . "attribute_description SET attribute_id = '" . (int)$attribute_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+		try {
+
+			$this->db->query("
+				INSERT INTO " . DB_PREFIX . "attribute 
+				SET 
+					`attribute_group_id` 	= '" . (int) $data['attribute_group_id'] . "', 
+					`sort_order` 					= '" . (int) $data['sort_order'] . "' 
+			");
+	
+			$attribute_id = $this->db->getLastId();
+	
+			foreach ($data['attribute_description'] as $language_id => $value) {
+				$this->db->query("
+					INSERT INTO " . DB_PREFIX . "attribute_description 
+					SET 
+						`attribute_id` 	= '" . (int) $attribute_id . "', 
+						`language_id` 	= '" . (int) $language_id . "', 
+						`store_id` 			= '" . (int) $this->session->data['store_id'] . "', 
+						`name` 					= '" . $this->db->escape($value['name']) . "'
+				");
+			}
+
+			// Stores association
+			if (isset($data['stores_association']) && !empty($data['stores_association'])) {
+				foreach ($data['stores_association'] as $store_id) {
+					$this->db->query("
+						INSERT INTO " . DB_PREFIX . "attribute_to_store
+						SET
+							`attribute_id` 				= '" . (int) $attribute_id . "', 
+							`attribute_group_id` 	= '" . (int) $data['attribute_group_id'] . "', 
+							`store_id` 		 				= '" . (int) $store_id . "',
+							`sort_order` 	 				= '" . (int) $data['sort_order'] . "'
+					");
+				}
+			}
+	
+			$this->db->query("COMMIT");
+			
+			return (int) $attribute_id;
+			
+		} catch (\Throwable $e) {
+
+			$this->db->query("ROLLBACK");
+
+			throw $e;
+			
 		}
-
-		return $attribute_id;
+		
 	}
 
 	public function editAttribute($attribute_id, $data) {
