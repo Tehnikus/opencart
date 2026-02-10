59b9a2b2670c5fe9883dbc688c2b1517ba6b0497
Tehnikus <tehnikus.ua@gmail.com>
2026-02-10 15:03:23 +0200

    Add store_id context to getTotalAttributeGroups()

--- upload/admin/model/catalog/attribute_group.php
+++ upload/admin/model/catalog/attribute_group.php
@@ -322,7 +322,10 @@ class ModelCatalogAttributeGroup extends Model {
 	}
 
 	public function getTotalAttributeGroups() {
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "attribute_group");
+		$query = $this->db->query("
+			SELECT COUNT(*) AS total 
+			FROM " . DB_PREFIX . "attribute_group
+		");
 
 		return $query->row['total'];
 	}
