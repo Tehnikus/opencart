9a213ecc41b190371941c340fae44f9e6f41b153
Tehnikus <tehnikus.ua@gmail.com>
2026-02-10 14:56:24 +0200

    addAttributeGroup() multistore overhaul

--- upload/admin/model/catalog/attribute_group.php
+++ upload/admin/model/catalog/attribute_group.php
@@ -1,15 +1,52 @@
 <?php
 class ModelCatalogAttributeGroup extends Model {
-	public function addAttributeGroup($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "attribute_group SET sort_order = '" . (int)$data['sort_order'] . "'");
+	public function addAttributeGroup($data) : int {
 
-		$attribute_group_id = $this->db->getLastId();
+		$this->db->query("START TRANSACTION");
+		
+		try {
+			$this->db->query("
+				INSERT INTO " . DB_PREFIX . "attribute_group 
+				SET 
+					sort_order = '" . (int) $data['sort_order'] . "'
+			");
 
-		foreach ($data['attribute_group_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO " . DB_PREFIX . "attribute_group_description SET attribute_group_id = '" . (int)$attribute_group_id . "', language_id = '" . (int)$language_id . "', name = '" . $this->db->escape($value['name']) . "'");
+			$attribute_group_id = $this->db->getLastId();
+
+			foreach ($data['attribute_group_description'] as $language_id => $value) {
+				$this->db->query("
+					INSERT INTO " . DB_PREFIX . "attribute_group_description 
+					SET 
+						attribute_group_id 	= '" . (int) $attribute_group_id . "', 
+						language_id 				= '" . (int) $language_id . "', 
+						store_id 						= '" . (int) $this->session->data['store_id'] . "',
+						name 								= '" . $this->db->escape($value['name']) . "'
+				");
+			}
+
+			// Stores association
+			if (isset($data['stores_association']) && !empty($data['stores_association'])) {
+				foreach ($data['stores_association'] as $store_id) {
+					$this->db->query("
+						INSERT INTO " . DB_PREFIX . "attribute_group_to_store
+						SET
+							`attribute_group_id` 	= '" . (int) $attribute_group_id . "', 
+							`store_id` 						= '" . (int) $store_id . "',
+							`sort_order` 					= '" . (int) $data['sort_order'] . "'
+					");
+				}
+			}
+
+			$this->db->query("COMMIT");
+
+			return $attribute_group_id;
+
+		} catch (\Throwable $e) {
+
+			$this->db->query("ROLLBACK");
+
+			throw $e;
 		}
-
-		return $attribute_group_id;
 	}
 
 	public function editAttributeGroup($attribute_group_id, $data) {
