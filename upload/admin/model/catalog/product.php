42e1bb56b6cfd0bd55c7bf0afd188c71e0cff2c5
Tehnikus <tehnikus.ua@gmail.com>
2026-02-10 02:49:40 +0200

    Add store_id context to getProductDiscounts() admin model

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -1428,7 +1428,13 @@ class ModelCatalogProduct extends Model {
 	}
 
 	public function getProductDiscounts($product_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_discount WHERE product_id = '" . (int)$product_id . "' ORDER BY quantity, priority, price");
+		$query = $this->db->query("
+			SELECT 
+				* 
+			FROM " . DB_PREFIX . "product_discount 
+			WHERE product_id  = '" . (int) $product_id . "' 
+				AND store_id 		= '" . (int) $this->session->data['store_id'] . "'
+			ORDER BY quantity, priority, price");
 
 		return $query->rows;
 	}
