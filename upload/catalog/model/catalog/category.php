578e25ced296e5311390bf0f2fbfc7fbbdce3b27
Tehnikus <tehnikus.ua@gmail.com>
2026-02-16 10:56:22 +0200

    Prettify code

--- upload/catalog/model/catalog/category.php
+++ upload/catalog/model/catalog/category.php
@@ -143,10 +143,16 @@ public function getCategoryFilters($category_id) {
 
 
 	public function getCategoryLayoutId($category_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category_to_layout WHERE category_id = '" . (int)$category_id . "' AND store_id = '" . (int)$this->config->get('config_store_id') . "'");
+		$query = $this->db->query("
+			SELECT 
+				* 
+			FROM " . DB_PREFIX . "category_to_layout 
+			WHERE category_id = '" . (int) $category_id . "' 
+				AND store_id 		= '" . (int) $this->config->get('config_store_id') . "'
+		");
 
 		if ($query->num_rows) {
-			return (int)$query->row['layout_id'];
+			return (int) $query->row['layout_id'];
 		} else {
 			return 0;
 		}
