d715300510a1837f553751e93e428fbcc4c2bd4a
Tehnikus <tehnikus.ua@gmail.com>
2026-02-16 10:54:31 +0200

    Rework frontend getCategories() to support multistore

--- upload/catalog/model/catalog/category.php
+++ upload/catalog/model/catalog/category.php
@@ -40,8 +40,35 @@ public function getCategory($category_id) : array {
 }
 
 
-	public function getCategories($parent_id = 0) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.parent_id = '" . (int)$parent_id . "' AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)");
+	public function getCategories($parent_id = 0) : array {
+
+		$sql = "
+			SELECT 
+				c.`category_id`,
+				c2s.`store_id`,
+				c2s.`parent_id`,
+				c2s.`sort_order`,
+				c2s.`status`,
+				c2s.`top`,
+				c2s.`image`,
+				c2s.`column`,
+				cd.`name`,
+				cd.`seo_keywords`,
+				cd.`date_modified`
+			FROM " . DB_PREFIX . "category_to_store c2s
+			INNER JOIN " . DB_PREFIX . "category c
+				ON  c.`category_id` = c2s.`category_id`
+			INNER JOIN " . DB_PREFIX . "category_description cd
+				ON  cd.`category_id` 	= c.category_id
+				AND cd.`language_id` 	= '" . (int) $this->config->get('config_language_id') . "'
+				AND cd.`store_id` 		= c2s.`store_id`
+			WHERE c2s.`parent_id` 	= '" . (int) $parent_id . "'
+				AND c2s.`store_id` 		= '" . (int) $this->config->get('config_store_id') . "'
+				AND c2s.`status` 			= 1
+			ORDER BY c2s.`sort_order` ASC
+		";
+
+		$query = $this->db->query($sql);
 
 		return $query->rows;
 	}
