c4955e8feb122bacc7fc422462e0827461d548c1
Tehnikus <tehnikus.ua@gmail.com>
2026-02-20 18:30:58 +0200

    Rework frontend getProductImages() for multistore support

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -501,8 +501,16 @@ class ModelCatalogProduct extends Model {
 		return $query->rows;
 	}
 
+	// Additional product images
 	public function getProductImages($product_id) {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_image WHERE product_id = '" . (int)$product_id . "' ORDER BY sort_order ASC");
+		$query = $this->db->query("
+			SELECT 
+				* 
+			FROM " . DB_PREFIX . "product_image 
+			WHERE product_id = '" . (int) $product_id . "' 
+				AND store_id   = '" . (int) $this->config->get('config_store_id') . "'
+			ORDER BY sort_order ASC
+		");
 
 		return $query->rows;
 	}
