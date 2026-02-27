294b6dd5205e752ecfffc2cb23a302e77be0e873
Tehnikus <tehnikus.ua@gmail.com>
2026-02-28 01:43:02 +0200

    Update translations

--- upload/admin/language/en-gb/extension/module/facet_filter.php
+++ upload/admin/language/en-gb/extension/module/facet_filter.php
@@ -3,16 +3,20 @@
 $_['heading_title']    = 'Facet filter';
 
 // Text
-$_['text_success']     = 'Success: You have modified filter module!';
+$_['text_success']     = 'Filter settings saved';
 $_['text_edit']        = 'Settings';
 
-$_['text_default_settings']   = 'Default settings';
-$_['text_add_category']       = 'Add category';
-$_['text_show_filters']       = 'Show filters';
-$_['text_show_options']       = 'Show options';
-$_['text_show_attributes']    = 'Show attributes';
-$_['text_show_manufacturers'] = 'Show manufacturers';
-$_['text_show_subcategories'] = 'Show subcategories';
+$_['text_manufacturer_settings']  = 'Manufacturer settings';
+$_['text_special_settings']       = 'Specials (discounts) settings';
+$_['text_search_settings']        = 'Search settings';
+$_['text_default_settings']       = 'Default category settings';
+$_['text_individual_categories']  = 'Individual category settings';
+$_['text_add_category']           = 'Add category';
+$_['text_show_filters']           = 'Show filters';
+$_['text_show_options']           = 'Show options';
+$_['text_show_attributes']        = 'Show attributes';
+$_['text_show_manufacturers']     = 'Show manufacturers';
+$_['text_show_subcategories']     = 'Show subcategories';
 
 // Entry
 $_['entry_status']     = 'Status';
