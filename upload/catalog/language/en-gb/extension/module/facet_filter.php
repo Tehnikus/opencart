72e6e70e4a86c5a19f3487e52de20eeb9bddaef3
Tehnikus <tehnikus.ua@gmail.com>
2026-03-04 00:24:06 +0200

    Update translations

--- upload/catalog/language/en-gb/extension/module/facet_filter.php
+++ upload/catalog/language/en-gb/extension/module/facet_filter.php
@@ -1,3 +1,17 @@
 <?php
-$_['heading_title']     = 'Filter';
-$_['text_manufacturers'] = 'Manufacturers';
+$_['heading_title']       = 'Filter';
+$_['text_manufacturers']  = 'Manufacturers';
+
+// Sort orders
+$_['sort_sort_order']     = 'Default';
+$_['sort_name']           = 'Name';
+$_['sort_price_asc']      = 'Price lowest';
+$_['sort_price_desc']     = 'Price highest';
+$_['sort_discounts']      = 'Discounts first';
+$_['sort_sales']          = 'Customer orders';
+$_['sort_rating']         = 'Rating';
+$_['sort_trends']         = 'Trending';
+$_['sort_views']          = 'Views';
+$_['sort_date_added']     = 'Latest';
+$_['sort_available']      = 'Available for order';
+$_['sort_quantity']       = 'Quantity';
