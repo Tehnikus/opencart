cf31bc2110cf288706d954aece65dc8552ec5ad5
Tehnikus <tehnikus.ua@gmail.com>
2026-03-09 19:27:47 +0200

    Write data to oc_product_stats table on product add/edit/delete

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -44,23 +44,6 @@ class ModelCatalogProduct extends Model {
 	
 			$product_id = $this->db->getLastId();
 
-			// Insert into product stats to sort products in frontend by various product features
-			$this->db->query("
-				INSERT INTO " . DB_PREFIX . "product_stats
-				SET 
-					`product_id` 	= '" . (int) $product_id . "',
-					`store_id`		= '" . (int) $this->session->data['store_id'] . "'
-			");
-			
-	
-			// if (isset($data['image'])) {
-			// 	$this->db->query("
-			// 		UPDATE " . DB_PREFIX . "product 
-			// 		SET 
-			// 			`image` = '" . $this->db->escape($data['image']) . "' 
-			// 		WHERE `product_id` = '" . (int) $product_id . "'
-			// 	");
-			// }
 	
 			foreach ($data['product_description'] as $language_id => $value) {
 				$this->db->query("
@@ -364,7 +347,52 @@ class ModelCatalogProduct extends Model {
 
 			// Build facet cache
 			$this->buildFacetIndex($product_id, $this->session->data['store_id']);
-			
+
+			// Insert into product stats to sort products in frontend by various product features
+			$this->db->query("
+				INSERT INTO " . DB_PREFIX . "product_stats
+				SET 
+					`product_id` 		= '" . (int) $product_id . "',
+					`store_id`			= '" . (int) $this->session->data['store_id'] . "',
+					`current_price` = (
+						SELECT
+							COALESCE(
+								(
+									SELECT 
+										ps.`price` 
+									FROM " . DB_PREFIX . "product_special ps
+									WHERE ps.`product_id` = p2s.`product_id`
+										AND ps.`store_id` = p2s.`store_id`
+										AND (
+											(ps.`date_start` = '0000-00-00' OR ps.`date_start` < NOW()) 
+											AND (ps.`date_end` = '0000-00-00' OR ps.`date_end` > NOW())
+										)
+								),
+								(
+									SELECT 
+										pd.price
+									FROM " . DB_PREFIX . "product_discount pd
+									WHERE pd.`product_id` = p2s.`product_id`
+										AND pd.`store_id` = p2s.`store_id`
+										AND (
+											(pd.`date_start` = '0000-00-00' OR pd.`date_start` < NOW()) 
+											AND (pd.`date_end` = '0000-00-00' OR pd.`date_end` > NOW())
+										)
+								),
+								p2s.`price`,
+								p.`price`
+							)
+							FROM " . DB_PREFIX . "product_to_store p2s
+							JOIN " . DB_PREFIX . "product p
+								ON p.`product_id` = p2s.`product_id`
+							WHERE p2s.`product_id` = '" . (int) $product_id . "'
+								AND p2s.`store_id` = '" . (int) $this->session->data['store_id'] . "'
+					),
+					`sort_order` = '" . $data['sort_order'] . "',
+					`date_added` = NOW()
+			");
+
+			// Commit DB queries
 			$this->db->query("COMMIT");
 
 			// Delete cache
@@ -422,14 +450,6 @@ class ModelCatalogProduct extends Model {
 					`image` 						= '" . (isset($data['image']) ? ($this->db->escape($data['image'])) : '') . "',
 					`date_modified`     = NOW() 
 				WHERE `product_id`    = '" . (int) $product_id . "'
-			");
-	
-			// Insert into product stats to sort products in frontend by various product features
-			$this->db->query("
-				INSERT IGNORE INTO " . DB_PREFIX . "product_stats
-				SET 
-					`product_id` 	= '" . (int) $product_id . "',
-					`store_id`		= '" . (int) $this->session->data['store_id'] . "'
 			");
 	
 			$this->db->query("
@@ -825,6 +845,60 @@ class ModelCatalogProduct extends Model {
 					$this->db->query("INSERT INTO " . DB_PREFIX . "product_to_layout SET `product_id` = '" . (int)$product_id . "', `store_id` = '" . (int)$store_id . "', `layout_id` = '" . (int)$layout_id . "'");
 				}
 			}
+
+			// Insert into product stats to sort products in frontend by various product features
+			$this->db->query("
+				INSERT INTO " . DB_PREFIX . "product_stats (product_id, store_id, current_price, sort_order, date_added)
+				VALUES (
+					'" . (int) $product_id . "',
+					'" . (int) $this->session->data['store_id'] . "',
+					(
+						SELECT
+							COALESCE(
+								(
+									SELECT 
+										ps.`price` 
+									FROM " . DB_PREFIX . "product_special ps
+									WHERE ps.`product_id` = p2s.`product_id`
+										AND ps.`store_id` = p2s.`store_id`
+										AND (
+											(ps.`date_start` = '0000-00-00' OR ps.`date_start` < NOW()) 
+											AND (ps.`date_end` = '0000-00-00' OR ps.`date_end` > NOW())
+										)
+									ORDER BY FIELD(ps.`customer_group_id`, '" . (int) $this->config->get('config_customer_group_id') . "'), ps.`priority`
+									LIMIT 1
+								),
+								(
+									SELECT 
+										pd.price
+									FROM " . DB_PREFIX . "product_discount pd
+									WHERE pd.`product_id` = p2s.`product_id`
+										AND pd.`store_id` = p2s.`store_id`
+										AND (
+											(pd.`date_start` = '0000-00-00' OR pd.`date_start` < NOW()) 
+											AND (pd.`date_end` = '0000-00-00' OR pd.`date_end` > NOW())
+										)
+									ORDER BY FIELD(pd.`customer_group_id`, '" . (int) $this->config->get('config_customer_group_id') . "'), pd.`quantity` ASC, pd.`priority` ASC
+									LIMIT 1
+								),
+								p2s.`price`,
+								p.`price`
+							)
+							FROM " . DB_PREFIX . "product_to_store p2s
+							JOIN " . DB_PREFIX . "product p
+								ON p.`product_id` = p2s.`product_id`
+							WHERE p2s.`product_id` = '" . (int) $product_id . "'
+								AND p2s.`store_id` = '" . (int) $this->session->data['store_id'] . "'
+					),
+					'" . $data['sort_order'] . "',
+					NOW()
+				)
+				ON DUPLICATE KEY UPDATE
+					`product_id` 		= VALUES(`product_id`),
+					`store_id`			= VALUES(`store_id`),
+					`current_price` = VALUES(`current_price`),
+					`sort_order` 		= VALUES(`sort_order`)
+			");
 			
 			// Build facet cache
 			$this->buildFacetIndex($product_id, $this->session->data['store_id']);
@@ -891,6 +965,7 @@ class ModelCatalogProduct extends Model {
 			'product_related',
 			'product_reward',
 			'product_special',
+			'product_stats',
 			'product_to_category',
 			'product_to_download',
 			'product_to_layout',
