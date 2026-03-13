aa6e7d9a303cddc43481a699cfb3a0b144552ffe
Tehnikus <tehnikus.ua@gmail.com>
2026-03-13 18:04:28 +0200

    Replace getProductSpecials() with facet filter request

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -924,74 +924,11 @@ class ModelCatalogProduct extends Model {
 	// 	return $products;
 	// }
 
-	public function getProductSpecials($data = array()) {
-
-	$store_id 					= (int) $this->config->get('config_store_id');
-	$language_id 				= (int) $this->config->get('config_language_id');
-	$limit 							= (int) $data['limit'];
-	$cacheName 					= "product.store_{$store_id}.language_{$language_id}.special.{$limit}";
-
-		$sql = "
-			SELECT 
-				DISTINCT ps.product_id, 
-				(SELECT AVG(rating) FROM " . DB_PREFIX . "review r1 WHERE r1.product_id = ps.product_id AND r1.status = '1' GROUP BY r1.product_id) AS rating 
-			FROM " . DB_PREFIX . "product_special ps 
-			LEFT JOIN " . DB_PREFIX . "product p ON (ps.product_id = p.product_id) 
-			LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) 
-			LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) 
-			WHERE p.status = '1' AND p.date_available <= NOW() 
-				AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' 
-				AND ps.customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' 
-				AND ((ps.date_start = '0000-00-00' OR ps.date_start < NOW()) 
-				AND (ps.date_end = '0000-00-00' OR ps.date_end > NOW())) 
-			GROUP BY ps.product_id
-		";
-
-		$sort_data = array(
-			'pd.name',
-			'p.model',
-			'ps.price',
-			'rating',
-			'p.sort_order'
-		);
-
-		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
-			if ($data['sort'] == 'pd.name' || $data['sort'] == 'p.model') {
-				$sql .= " ORDER BY LCASE(" . $data['sort'] . ")";
-			} else {
-				$sql .= " ORDER BY " . $data['sort'];
-			}
-		} else {
-			$sql .= " ORDER BY p.sort_order";
-		}
-
-		if (isset($data['order']) && ($data['order'] == 'DESC')) {
-			$sql .= " DESC, LCASE(pd.name) DESC";
-		} else {
-			$sql .= " ASC, LCASE(pd.name) ASC";
-		}
-
-		if (isset($data['start']) || isset($data['limit'])) {
-			if ($data['start'] < 0) {
-				$data['start'] = 0;
-			}
-
-			if ($data['limit'] < 1) {
-				$data['limit'] = 20;
-			}
-
-			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
-		}
-
-		$product_data = array();
-
-		$query = $this->db->query($sql);
-
-		foreach ($query->rows as $result) {
-			$product_data[$result['product_id']] = $this->getProduct($result['product_id']);
-		}
-
-		return $product_data;
+	
+	public function getProductSpecials($data = []) : array {
+		$data['filter_has_discount'] = 1;
+		$productData = $this->getProducts($data);
+		return $productData;
 	}
 
 	public function getLatestProducts($limit) {
