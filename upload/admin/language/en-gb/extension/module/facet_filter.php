cdc15962f814607da3d3107317966ae75a4da7c2
Tehnikus <tehnikus.ua@gmail.com>
2026-03-17 02:01:01 +0200

    Update translations

--- upload/admin/language/en-gb/extension/module/facet_filter.php
+++ upload/admin/language/en-gb/extension/module/facet_filter.php
@@ -9,15 +9,20 @@ $_['text_edit']        = 'Settings';
 $_['text_manufacturer_settings']  = 'Manufacturer settings';
 $_['text_special_settings']       = 'Specials (discounts) settings';
 $_['text_search_settings']        = 'Search settings';
-$_['text_default_settings']       = 'Default category settings';
+$_['text_category_settings']      = 'Default category settings';
+
+$_['text_show_filter']            = 'Filters';
+$_['text_show_option']            = 'Options';
+$_['text_show_attribute']         = 'Attributes';
+$_['text_show_manufacturer_id']   = 'Manufacturers';
+$_['text_show_category_id']       = 'Subcategories';
+$_['text_show_is_featured']       = 'Featured';
+$_['text_show_is_available']      = 'Available';
+$_['text_show_has_discount']      = 'Has discount';
+
+
 $_['text_individual_categories']  = 'Individual category settings';
 $_['text_add_category']           = 'Add category';
-$_['text_show_filters']           = 'Show filters';
-$_['text_show_options']           = 'Show options';
-$_['text_show_attributes']        = 'Show attributes';
-$_['text_show_manufacturers']     = 'Show manufacturers';
-$_['text_show_subcategories']     = 'Show subcategories';
-
 // Entry
 $_['entry_status']     = 'Status';
 $_['entry_cache']      = 'Cache filter list';
