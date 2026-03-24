c177ef452728487ad9d4fa919bebcb9868fe8e23
Tehnikus <tehnikus.ua@gmail.com>
2026-03-24 14:38:08 +0200

    Remove deprecated VALUES() from SQL

--- upload/catalog/model/checkout/order.php
+++ upload/catalog/model/checkout/order.php
@@ -16,7 +16,7 @@ class ModelCheckoutOrder extends Model {
 					$this->db->query("INSERT INTO " . DB_PREFIX . "order_option SET order_id = '" . (int)$order_id . "', order_product_id = '" . (int)$order_product_id . "', product_option_id = '" . (int)$option['product_option_id'] . "', product_option_value_id = '" . (int)$option['product_option_value_id'] . "', name = '" . $this->db->escape($option['name']) . "', `value` = '" . $this->db->escape($option['value']) . "', `type` = '" . $this->db->escape($option['type']) . "'");
 				}
 
-				// Update product stats
+				// Update product sorts
 				$this->db->query("
 					INSERT INTO " . DB_PREFIX . "facet_sort (`product_id`, `store_id`, `orders`, `date_last_order`)
 					VALUES (
@@ -65,7 +65,7 @@ class ModelCheckoutOrder extends Model {
 
 		$this->db->query("UPDATE `" . DB_PREFIX . "order` SET invoice_prefix = '" . $this->db->escape($data['invoice_prefix']) . "', store_id = '" . (int)$data['store_id'] . "', store_name = '" . $this->db->escape($data['store_name']) . "', store_url = '" . $this->db->escape($data['store_url']) . "', customer_id = '" . (int)$data['customer_id'] . "', customer_group_id = '" . (int)$data['customer_group_id'] . "', firstname = '" . $this->db->escape($data['firstname']) . "', lastname = '" . $this->db->escape($data['lastname']) . "', email = '" . $this->db->escape($data['email']) . "', telephone = '" . $this->db->escape($data['telephone']) . "', custom_field = '" . $this->db->escape(json_encode($data['custom_field'])) . "', payment_firstname = '" . $this->db->escape($data['payment_firstname']) . "', payment_lastname = '" . $this->db->escape($data['payment_lastname']) . "', payment_company = '" . $this->db->escape($data['payment_company']) . "', payment_address_1 = '" . $this->db->escape($data['payment_address_1']) . "', payment_address_2 = '" . $this->db->escape($data['payment_address_2']) . "', payment_city = '" . $this->db->escape($data['payment_city']) . "', payment_postcode = '" . $this->db->escape($data['payment_postcode']) . "', payment_country = '" . $this->db->escape($data['payment_country']) . "', payment_country_id = '" . (int)$data['payment_country_id'] . "', payment_zone = '" . $this->db->escape($data['payment_zone']) . "', payment_zone_id = '" . (int)$data['payment_zone_id'] . "', payment_address_format = '" . $this->db->escape($data['payment_address_format']) . "', payment_custom_field = '" . $this->db->escape(json_encode($data['payment_custom_field'])) . "', payment_method = '" . $this->db->escape($data['payment_method']) . "', payment_code = '" . $this->db->escape($data['payment_code']) . "', shipping_firstname = '" . $this->db->escape($data['shipping_firstname']) . "', shipping_lastname = '" . $this->db->escape($data['shipping_lastname']) . "', shipping_company = '" . $this->db->escape($data['shipping_company']) . "', shipping_address_1 = '" . $this->db->escape($data['shipping_address_1']) . "', shipping_address_2 = '" . $this->db->escape($data['shipping_address_2']) . "', shipping_city = '" . $this->db->escape($data['shipping_city']) . "', shipping_postcode = '" . $this->db->escape($data['shipping_postcode']) . "', shipping_country = '" . $this->db->escape($data['shipping_country']) . "', shipping_country_id = '" . (int)$data['shipping_country_id'] . "', shipping_zone = '" . $this->db->escape($data['shipping_zone']) . "', shipping_zone_id = '" . (int)$data['shipping_zone_id'] . "', shipping_address_format = '" . $this->db->escape($data['shipping_address_format']) . "', shipping_custom_field = '" . $this->db->escape(json_encode($data['shipping_custom_field'])) . "', shipping_method = '" . $this->db->escape($data['shipping_method']) . "', shipping_code = '" . $this->db->escape($data['shipping_code']) . "', comment = '" . $this->db->escape($data['comment']) . "', total = '" . (float)$data['total'] . "', affiliate_id = '" . (int)$data['affiliate_id'] . "', commission = '" . (float)$data['commission'] . "', date_modified = NOW() WHERE order_id = '" . (int)$order_id . "'");
 
-		// Update product stats
+		// Update product sorts
 		foreach ($data['products'] as $product) {
 			// Get old quantity
 			$old_quantity = $this->db->query("
@@ -82,17 +82,19 @@ class ModelCheckoutOrder extends Model {
 			// Skip if no difference
 			if ($delta === 0) continue;
 
-			// Update product stats
+			// Update product sorts
 			$this->db->query("
-				INSERT INTO " . DB_PREFIX . "facet_sort (`product_id`, `store_id`, `orders`, `date_last_order`)
-				VALUES (
-					'" . (int) $product['product_id'] . "',
-					'" . (int) $data['store_id'] . "',
-					'" . (int) $delta . "',
-					NOW()
-				)
+				INSERT INTO " . DB_PREFIX . "facet_sort AS new
+					(`product_id`, `store_id`, `orders`, `date_last_order`)
+					VALUES (
+							'" . (int) $product['product_id'] . "',
+							'" . (int) $data['store_id'] . "',
+							'" . (int) $delta . "',
+							NOW()
+					)
 				ON DUPLICATE KEY UPDATE
-					`orders` = `orders` + VALUES(`orders`)
+					`orders` = COALESCE(`orders`, 0) + new.`orders`
+					`date_last_order` = new.`date_last_order`
 			");
 		}
 
@@ -146,7 +148,7 @@ class ModelCheckoutOrder extends Model {
 		// Void the order first
 		$this->addOrderHistory($order_id, 0);
 
-		// Update product stats
+		// Update product sorts
 		// Get order store_id
     $order = $this->db->query("
 			SELECT store_id
@@ -164,17 +166,18 @@ class ModelCheckoutOrder extends Model {
 			WHERE order_id = '" . (int) $order_id . "'
     ")->rows;
 
-    // Decrease product sales stats
+    // Decrease product sales sorts
     foreach ($products as $product) {
 			$this->db->query("
-				INSERT INTO " . DB_PREFIX . "facet_sort (`product_id`, `store_id`, `orders`)
+				INSERT INTO " . DB_PREFIX . "facet_sort AS new
+				(`product_id`, `store_id`, `orders`) 
 				VALUES (
 					'" . (int) $product['product_id'] . "',
 					'" . $store_id . "',
 					'" . (int) (-$product['quantity']) . "'
 				)
 				ON DUPLICATE KEY UPDATE
-					`orders` = `orders` + VALUES(`orders`)
+					`orders` = COALESCE(`orders`, 0) + new.`orders`
 			");
     }
 
