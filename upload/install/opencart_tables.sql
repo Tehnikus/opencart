072a052d3b98b69bb56e98023fcf9bdbf37d83ec
Tehnikus <tehnikus.ua@gmail.com>
2026-03-27 01:00:33 +0200

    Add oc_keywords table

--- upload/install/opencart_tables.sql
+++ upload/install/opencart_tables.sql
@@ -7466,5 +7466,16 @@ CREATE TABLE `oc_search_page_image_description` (
   `language_id`             INT NOT NULL,
   `store_id`                INT NOT NULL,
   `description`             TEXT NOT NULL,
-  KEY (`search_page_id`, `language_id`, `store_id`)
+  PRIMARY KEY (`search_page_id`, `language_id`, `store_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `oc_keywords`;
+CREATE TABLE `oc_keywords` (
+  `keyword_id`      INT NOT NULL AUTO_INCREMENT,
+  `keyword_text`    VARCHAR(255) NOT NULL,
+  `keyword_url`     VARCHAR(255) NOT NULL,
+  `language_id`     INT NOT NULL,
+  `store_id`        INT NOT NULL,
+  PRIMARY KEY (`keyword_id`),
+  KEY (`language_id`, `store_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
