8853680ed156ddd326fb6a82f451a3a14757fb3d
Tehnikus <tehnikus.ua@gmail.com>
2026-03-28 02:23:23 +0200

    Куьщму гтгыув сщву

--- upload/catalog/model/catalog/category.php
+++ upload/catalog/model/catalog/category.php
@@ -185,19 +185,4 @@ class ModelCatalogCategory extends Model {
 			return 0;
 		}
 	}
-
-	public function getTotalCategoriesByCategoryId($parent_id = 0) {
-		$query = $this->db->query("
-			SELECT 
-				COUNT(c2s.category_id) AS total 
-			FROM " . DB_PREFIX . "category_to_store c2s
-			INNER JOIN " . DB_PREFIX . "category c
-				ON c.category_id = c2s.category_id
-			WHERE c2s.parent_id = '" . (int) $parent_id . "' 
-				AND c2s.store_id 	= '" . (int) $this->config->get('config_store_id') . "' 
-				AND c2s.status 		= '1'
-		");
-
-		return $query->row['total'];
-	}
 }
